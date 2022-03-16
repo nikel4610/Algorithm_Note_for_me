e22774b10123b04fcdfbb1be8e8eb2eab1c44e65
Hyun - Woo <nikel4610@gmail.com>
2022-03-16 15:52:41 +0900

    Update 11.py

--- 11.py
+++ 11.py
@@ -4,3 +4,10 @@ t = int(input()) # 테스트 케이스 개수 t개
 for _ in range(t):
   a, b = map(int,input().split())
   ~~
+
+ # 1부터 n 까지의 합 출력
+ n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print(sum)
