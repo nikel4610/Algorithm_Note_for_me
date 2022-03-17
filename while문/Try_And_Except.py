79365e183fd009e41adc89b2275475fe5168b3e9
Hyun - Woo <nikel4610@gmail.com>
2022-03-17 15:44:32 +0900

    Update Try_And_Except.py

--- while문/Try_And_Except.py
+++ while문/Try_And_Except.py
@@ -5,5 +5,7 @@ while True:
     try:
         a, b = map(int, input().split())
         print(a + b)
-    except:
+    except: # 더이상 테스트 케이스가 입력되지 않으면 except로 넘어가서 break
         break
+# 정해놓은 발생오류가 발생하면 메시지와 함께 except문 수행하려면
+# except 발생오류 as 메시지 :
