a062b58ababbc20d165834eb1a0be8061e65ec18
Hyun - Woo <nikel4610@gmail.com>
2022-04-06 17:05:16 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -5,3 +5,26 @@ list.sort(reverse = True) # 거꾸로 정렬
 list.sort(key = lambda x:x[n]) # x[n] 기준으로 정렬해줌
 # (x[n], x[n+1])
 # n -> 정수
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+list = []
+count = 1
+
+for i in range(n):
+    a, b = map(int, input().split())
+    list.append((a, b))
+
+list.sort(key = lambda x:(x[1], x[0]))
+
+end = list[0][1]
+for i in range(1, n):
+    if list[i][0] >= end:
+        count += 1
+        end = list[i][1]
+
+print(count)
+
+# 백준 1931 회의실 배정
