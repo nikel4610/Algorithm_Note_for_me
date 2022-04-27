91807dfefe3bdf8a16e908db9acfdc8061057e77
Hyun - Woo <nikel4610@gmail.com>
2022-04-27 18:03:30 +0900

    Update 퀵정렬.py

--- sort/퀵정렬.py
+++ sort/퀵정렬.py
@@ -28,3 +28,25 @@ def quick_sort(array, start, end):
 
 quick_sort(array, 0, len(array) - 1)
 print(array)
+
+
+### 더 파이썬스러움
+import sys
+input = sys.stdin.readline
+
+array = [...]
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+    pivot = array[0] # 첫번째 원소(피벗)
+    tail = array[1:] # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행 후, 전체 리스트 반환   
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
