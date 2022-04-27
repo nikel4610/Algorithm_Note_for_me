fb34cf92330d932a2bdbd47654e7799000ce30b2
Hyun - Woo <nikel4610@gmail.com>
2022-04-27 18:10:49 +0900

    Create 계수 정렬.py

---
+++ sort/계수 정렬.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+# 계수 정렬
+array = [...]
+
+# 모든 범위를 포함하는 리스트 선언(모든 값을 0으로 초기화)
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1 # 각 데이터에 해당하는 인덱스 값 증가
+
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]):
+        print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
