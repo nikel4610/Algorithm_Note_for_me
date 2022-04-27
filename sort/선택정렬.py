b26dfab668322d328208a36e2bd8ce0f0f693202
Hyun - Woo <nikel4610@gmail.com>
2022-04-27 17:31:21 +0900

    Create 선택정렬.py

---
+++ sort/선택정렬.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+array = [...]
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # 자리 바꿈
+    
+print(array)
