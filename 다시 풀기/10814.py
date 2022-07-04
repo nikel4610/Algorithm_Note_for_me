be67f8e3e062dfb243be894791cdff239d963e69
nikel4610 <nikel4610@gmail.com>
2022-07-04 15:12:59 +0900

    백준 정렬 다시풀기

---
+++ 다시 풀기/10814.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline # 기본
+
+n = int(input())
+list = []
+
+for i in range(n):
+    a, b = map(str, input().split())
+    list.append([int(a), i, b])
+
+list.sort()
+for i in range(len(list)):
+    print("%d %s"%(list[i][0], list[i][2]))
--- 다시 풀기/sortstudy.py
+++ 다시 풀기/sortstudy.py
@@ -7,8 +7,10 @@ list = []
 for i in range(n):
     a, b = map(int, input().split())
     list.append([a, b]) # 2중 리스트
+    # [[3, 4], [1, 1], [1, -1], ...] 이런식으로 출력됨
 
 result = sorted(list)
 
 for i in range(n):
     print(result[i][0], result[i][1])
+    # i에 0부터 쭉쭉 돌아가며 인덱싱
